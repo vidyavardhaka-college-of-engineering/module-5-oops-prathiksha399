7379cf27be9ae3a714c9c70fbefb17a3d1db1b55
prathiksha399 <prathikshaa0803@gmail.com>
2021-03-30 10:59:38 +0000

    done

--- Program-10.cpp
+++ Program-10.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int main()
 {
-double x = 10.25,   ;//complete the declaration
-res =; //use the function with gives us the square root
+double x = 10.25, res;//complete the declaration
+res =sqrt(x); //use the function with gives us the square root
 cout << res;
 return 0;
 }
